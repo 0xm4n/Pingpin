3879db329589caa808cd469c7b73f8b170d2e93a
roffen <ohroffen@foxmail.com>
2018-08-29 22:59:17 +0800

    修改导航栏通过滑动适应不同屏幕

--- assets/src/apply.js
+++ assets/src/apply.js
@@ -286,3 +286,13 @@ function Page(opt){
         clickPages.newPages("jump",set.startNum)
     }
 }
+
+// 添加监视scroll的监听器，为导航栏添加一个负的left定位
+ document.addEventListener("scroll",setPosition);
+ window.addEventListener("resize",setPosition);
+ function setPosition(){
+    //  var scroll_left=document.body.scrollLeft;
+    // 在chrome浏览器中不是document.body
+    var scroll_left=document.documentElement.scrollLeft;
+     $("#header").css('left',~scroll_left+1);
+ }
